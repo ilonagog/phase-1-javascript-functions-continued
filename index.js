6b0f48b6abb9e1608131d4180026e426a21716c2
ilona <gogiashvili.ilona@gmail.com>
2024-04-11 22:44:23 -0400

    commit tree 58befc51e3b3d30e157dcc950fe6885c5fbe5843

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function saturdayFun(task = "roller-skate") {
+  return `This Saturday, I want to ${task}!`;
+}
+
+function mondayWork(task = "go to the office") {
+  return `This Monday, I will ${task}.`;
+}
+function wrapAdjective(adj = "special") {
+  return `You are ${adj}!`;
+}
